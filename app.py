0e994784331e77a5849f311d41742758da9aed04
Doğukan Ürker <62756402+DogukanUrker@users.noreply.github.com>
2022-11-17 19:33:42 +0300

    articles
    im working on articles backend.

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
+import json
 app = Flask(__name__)
-
+f = open("articles.json")
+data = json.load(f)
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",title=data["title"],date=data["date"],content=data["content"])
+
+@app.route("/{{articleID}}")
+def article():
+    f = open("articles/article.json")
+    data = json.load(f)
+    return render_template("article.html",title=data["title"],date=data["date"],content=data["content"])
 
 
 if __name__ == "__main__":
