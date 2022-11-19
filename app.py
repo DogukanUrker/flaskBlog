cbfb99588e5840208aeb14c270106ef9708e2513
Doğukan Ürker <62756402+DogukanUrker@users.noreply.github.com>
2022-11-19 22:29:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,34 @@
-from flask import Flask, render_template,redirect
+from flask import Flask, render_template, redirect
 import json
 import os
+
 app = Flask(__name__)
+
+posts = []
+for postTitle in os.listdir("posts/"):
+    posts.append(postTitle.rsplit(".", 1)[0])
 
 
 @app.route("/")
 def index():
-    return render_template("index.html",title=os.listdir("posts/"))
-
-
-
+    return render_template("index.html", title=posts)
 
 
 @app.route("/<postID>")
 def post(postID):
     try:
-        f = open("posts/{}".format(postID))
+        post = open("posts/{}".format(postID))
     except:
         return redirect("/404")
-    data = json.load(f)
-    return render_template("post.html",title=data["title"],date=data["date"],content=data["content"])
+    data = json.load(post)
+    return render_template(
+        "post.html", title=data["title"], date=data["date"], content=data["content"]
+    )
 
 
-@app.route("/404")
+@app.errorhandler(404)
 def page_not_found():
-    return "404"
-
+    return "404", 404
 
 
 if __name__ == "__main__":
