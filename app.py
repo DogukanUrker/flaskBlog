adb1b3c888351b2eafc6ec3dd71a5f94aab80573
Doğukan Ürker <62756402+DogukanUrker@users.noreply.github.com>
2022-11-24 21:02:47 +0300

    im trying database thing 💀💀💀

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template
-import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 import json
 import os
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flaskBlog.db"
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+    db.session.add(User(username="example"))
+    db.session.commit()
+
+    users = db.session.execute(db.select(User)).scalars()
 
 posts = []
 for postTitle in os.listdir("posts/"):
