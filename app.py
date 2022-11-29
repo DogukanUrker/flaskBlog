542153319ad29f5cc85764059f0265d89b883932
Doğukan Ürker <62756402+DogukanUrker@users.noreply.github.com>
2022-11-29 19:22:22 +0300

    db added

--- app.py
+++ app.py
@@ -2,8 +2,22 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 theme = "dark"
-
+db = SQLAlchemy()
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+db.init_app(app)
+
+
+class User(db.Model):
+    userID = db.Column(db.Integer, primary_key=True)
+    userName = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String)
+    posts = db.Column(db.String)
+    password = db.Column(db.String, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
