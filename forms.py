e4faa915d8d3273a1cda001a34e025b005ca225f
Doğukan Ürker <62756402+DogukanUrker@users.noreply.github.com>
2023-01-13 00:52:09 +0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,34 @@
 from wtforms import Form, PasswordField, StringField, TextAreaField, validators
 
 
+class loginForm(Form):
+    userName = StringField(
+        "Username",
+        [validators.Length(min=4, max=25), validators.InputRequired()],
+        render_kw={"placeholder": "username"},
+    )
+    password = PasswordField(
+        "Password",
+        [validators.Length(min=8), validators.InputRequired()],
+        render_kw={"placeholder": "password"},
+    )
+
+
+class createPostForm(Form):
+    postTitle = StringField(
+        "Post Title",
+        [validators.Length(min=4, max=75), validators.InputRequired()],
+        render_kw={"placeholder": "post title"},
+    )
+    postTags = StringField(
+        "Post Tags", [validators.InputRequired()], render_kw={"placeholder": "tags"}
+    )
+    postContent = TextAreaField(
+        "Post Content",
+        [validators.Length(min=50), validators.InputRequired()],
+    )
+
+
 class signUpForm(Form):
     userName = StringField(
         "Username",
@@ -27,32 +55,4 @@ class signUpForm(Form):
             validators.InputRequired(),
         ],
         render_kw={"placeholder": "confirm your password"},
-    )
-
-
-class loginForm(Form):
-    userName = StringField(
-        "Username",
-        [validators.Length(min=4, max=25), validators.InputRequired()],
-        render_kw={"placeholder": "username"},
-    )
-    password = PasswordField(
-        "Password",
-        [validators.Length(min=8), validators.InputRequired()],
-        render_kw={"placeholder": "password"},
-    )
-
-
-class createPostForm(Form):
-    postTitle = StringField(
-        "Post Title",
-        [validators.Length(min=4, max=75), validators.InputRequired()],
-        render_kw={"placeholder": "post title"},
-    )
-    postTags = StringField(
-        "Post Tags", [validators.InputRequired()], render_kw={"placeholder": "tags"}
-    )
-    postContent = TextAreaField(
-        "Post Content",
-        [validators.Length(min=50), validators.InputRequired()],
     )
