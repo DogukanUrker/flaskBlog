96dd07918d95cc53f5d560d379c69dd5263a0ae2
Doğukan Ürker <62756402+DogukanUrker@users.noreply.github.com>
2023-01-24 22:08:57 +0300

    flickering fixed
    flickering fixed and less code

--- static/js/changeTheme.js
+++ static/js/changeTheme.js
@@ -1,36 +1,27 @@
 var root = document.querySelector(":root");
 var rootStyles = getComputedStyle(root);
-var btnLight = document.querySelector(".toLight");
-var btnDark = document.querySelector(".toDark");
+var changeTheme = document.querySelector(".changeTheme");
 
-theme = "light";
+theme = "dark";
 
 if (localStorage.getItem(theme) === "light") {
-  root.style.setProperty("--dark", "#000");
-  root.style.setProperty("--light", "#fff");
-  btnDark.style.display = "inline-block";
-  btnLight.style.display = "none";
+  toLight()
 } else if (localStorage.getItem(theme) === "dark") {
-  root.style.setProperty("--dark", "#fff");
-  root.style.setProperty("--light", "#000");
-  btnLight.style.display = "inline-block";
-  btnDark.style.display = "none";
+  toDark()
 }
 
 function toLight() {
   localStorage.setItem(theme, "light");
   root.style.setProperty("--dark", "#000");
   root.style.setProperty("--light", "#fff");
-  btnLight.style.display = "none";
-  btnDark.innerHTML = "🌚";
-  btnDark.style.display = "inline-block";
+  changeTheme.innerHTML = "🌚";
+  changeTheme.setAttribute("onclick", "javascript: toDark();")
 }
 
 function toDark() {
   localStorage.setItem(theme, "dark");
   root.style.setProperty("--dark", "#fff");
   root.style.setProperty("--light", "#000");
-  btnDark.style.display = "none";
-  btnLight.innerHTML = "☀️";
-  btnLight.style.display = "inline-block";
+  changeTheme.innerHTML = "🌞";
+  changeTheme.setAttribute("onclick", "javascript: toLight();")
 }
