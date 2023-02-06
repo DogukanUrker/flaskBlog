48f4a2aa96850add2b1a52149ac5ea3d86b5fbac
Doğukan Ürker <62756402+DogukanUrker@users.noreply.github.com>
2023-02-07 00:08:31 +0300

    module name and message color changed

--- dbChecker.py
+++ dbChecker.py
@@ -1,32 +1,32 @@
 from helpers import mkdir, exists, message, sqlite3
 
 
-def dbDirectory():
+def dbFolder():
     match exists("db"):
         case True:
-            message("2", '"/db" FOUND')
+            message("6", 'Folder: "/db" FOUND')
         case False:
-            message("1", '"/db" NOT FOUND')
+            message("1", 'Folder: "/db" NOT FOUND')
             mkdir("db")
-            message("2", '"/db" CREATED')
+            message("2", 'Folder: "/db" CREATED')
 
 
 def usersTable():
     match exists("db/users.db"):
         case True:
-            message("2", '"users.db" FOUND')
+            message("6", 'DATABASE: "users.db" FOUND')
         case False:
-            message("1", '"users.db" NOT FOUND')
+            message("1", 'DATABASE: "users.db" NOT FOUND')
             open("db/users.db", "x")
-            message("2", '"users.db" CREATED')
+            message("2", 'DATABASE: "users.db" CREATED')
     connection = sqlite3.connect("db/users.db")
     cursor = connection.cursor()
     try:
         cursor.execute("""SELECT * FROM users; """).fetchall()
-        message("2", '"Users" TABLE FOUND')
+        message("6", 'TABLE: "Users" FOUND')
         connection.close()
     except:
-        message("1", '"Users" TABLE NOT FOUND')
+        message("1", 'TABLE: "Users" NOT FOUND')
         usersTable = """
         CREATE TABLE IF NOT EXISTS Users(
 	    "userID"	INTEGER NOT NULL UNIQUE,
@@ -42,25 +42,25 @@ def usersTable():
         cursor.execute(usersTable)
         connection.commit()
         connection.close()
-        message("2", '"Users" TABLE CREATED')
+        message("2", 'TABLE: "Users" CREATED')
 
 
 def postsTable():
     match exists("db/posts.db"):
         case True:
-            message("2", '"posts.db" FOUND')
+            message("6", 'DATABASE: "posts.db" FOUND')
         case False:
-            message("1", '"posts.db" NOT FOUND')
+            message("1", 'DATABASE: "posts.db" NOT FOUND')
             open("db/posts.db", "x")
-            message("2", '"posts.db" CREATED')
+            message("2", 'DATABASE: "posts.db" CREATED')
     connection = sqlite3.connect("db/posts.db")
     cursor = connection.cursor()
     try:
         cursor.execute("""SELECT * FROM posts; """).fetchall()
-        message("2", '"Posts" TABLE FOUND')
+        message("6", 'TABLE: "Posts" FOUND')
         connection.close()
     except:
-        message("1", '"Posts" TABLE NOT FOUND')
+        message("1", 'TABLE: "Posts" NOT FOUND')
         postsTable = """
         CREATE TABLE "posts" (
     	"id"	INTEGER NOT NULL UNIQUE,
@@ -78,25 +78,25 @@ def postsTable():
         cursor.execute(postsTable)
         connection.commit()
         connection.close()
-        message("2", '"Posts" TABLE CREATED')
+        message("2", 'TABLE: "Posts" CREATED')
 
 
 def commentsTable():
     match exists("db/comments.db"):
         case True:
-            message("2", '"comments.db" FOUND')
+            message("6", 'DATABASE: "comments.db" FOUND')
         case False:
-            message("1", '"comments.db" NOT FOUND')
+            message("1", 'DATABASE: "comments.db" NOT FOUND')
             open("db/comments.db", "x")
-            message("2", '"comments.db" CREATED')
+            message("2", 'DATABASE: "comments.db" CREATED')
     connection = sqlite3.connect("db/comments.db")
     cursor = connection.cursor()
     try:
         cursor.execute("""SELECT * FROM comments; """).fetchall()
-        message("2", '"Comments" TABLE FOUND')
+        message("6", 'TABLE: "Comments" FOUND')
         connection.close()
     except:
-        message("1", '"Comments" TABLE NOT FOUND')
+        message("1", 'TABLE: "Comments" NOT FOUND')
         commentsTable = """
         CREATE TABLE IF NOT EXISTS comments(
 	    "id"	INTEGER NOT NULL,
@@ -110,4 +110,4 @@ def commentsTable():
         cursor.execute(commentsTable)
         connection.commit()
         connection.close()
-        message("2", '"Comments" TABLE CREATED')
+        message("2", 'TABLE: "Comments" CREATED')
