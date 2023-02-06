788f20cb724320548b29879e170ba9c44cf354ef
Doğukan Ürker <62756402+DogukanUrker@users.noreply.github.com>
2023-02-07 00:09:31 +0300

    unnecessary code removed

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,7 +1,6 @@
 function search() {
     var input = document.getElementById("searchInput").value;
     if (input === "" || input.replace(/\s/g, "") === "") {
-        console.log("empty")
     } else {
         window.location.href = `/search/${input.replace(/\s/g, "+")}`;
     }
