37335b656d1ec6d961e0d00896a5664cce736efd
Doğukan Ürker <62756402+DogukanUrker@users.noreply.github.com>
2023-02-08 11:42:45 +0300

    added hamburger button and responsive design improvements

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -5,3 +5,19 @@ function search() {
         window.location.href = `/search/${input.replace(/\s/g, "+")}`;
     }
 }
+function hamburger() {
+    document.getElementById("hamburgerDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('.hamburgerBtn')) {
+        var dropdowns = document.getElementsByClassName("hamburgerContent");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
