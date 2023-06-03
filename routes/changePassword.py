4b610db61a4d6abf27fe451e913531b8ba34c0a8
Doğukan Ürker <dogukanurker@icloud.com>
2023-06-03 19:18:50 +0300

    wrong URL redirecting fixed

--- routes/changePassword.py
+++ routes/changePassword.py
@@ -30,10 +30,8 @@ def changePassword():
                 )
                 if sha256_crypt.verify(oldPassword, cursor.fetchone()[0]):
                     if oldPassword == password:
-                        flash("new password cant be same with old password", "error")
-                        message("1", "NEW PASSWORD CANT BE SAME WITH OLD PASSWORD")
+                        flash("new password can not be same with old password", "error")
                     elif password != passwordConfirm:
-                        message("1", "PASSWORDS MUST MATCH")
                         flash("passwords must match", "error")
                     elif oldPassword != password and password == passwordConfirm:
                         newPassword = sha256_crypt.hash(password)
@@ -48,13 +46,12 @@ def changePassword():
                         )
                         session.clear()
                         flash("you need login with new password", "success")
-                        return redirect("/login")
+                        return redirect("/login/redirect=&")
                 else:
                     flash("old password wrong", "error")
-                    message("1", "OLD PASSWORD WRONG")
 
             return render_template("changePassword.html", form=form)
         case False:
             message("1", "USER NOT LOGGED IN")
             flash("you need login for change your password", "error")
-            return redirect("/login")
+            return redirect("/login/redirect=changepassword")
