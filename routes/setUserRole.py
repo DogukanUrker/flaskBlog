7cadc20afc26e52c39799af08abd3e34f1944333
Doğukan Ürker <dogukanurker@icloud.com>
2023-12-29 00:18:34 +0300

    formatted code

--- routes/setUserRole.py
+++ routes/setUserRole.py
@@ -1,10 +1,10 @@
-from helpers import sqlite3, Blueprint, session, redirect,message
+from helpers import sqlite3, Blueprint, session, redirect, message
 
 setUserRoleBlueprint = Blueprint("setUserRole", __name__)
 
 
 @setUserRoleBlueprint.route("/setuserrole/<userName>/<newRole>")
-def setUserRole(userName,newRole):
+def setUserRole(userName, newRole):
     match "userName" in session:
         case True:
             connection = sqlite3.connect("db/users.db")
@@ -14,12 +14,17 @@ def setUserRole(userName,newRole):
             )
             role = cursor.fetchone()[0]
             match role == "admin":
-                case  True:
-                    cursor.execute(f'update users set role = "{newRole}" where lower(userName) = "{userName.lower()}" ')
+                case True:
+                    cursor.execute(
+                        f'update users set role = "{newRole}" where lower(userName) = "{userName.lower()}" '
+                    )
                     connection.commit()
-                    message("2",f'ADMIN: "{session["userName"]}" CHANGED USER: "{userName}"s ROLE TO {newRole}')
+                    message(
+                        "2",
+                        f'ADMIN: "{session["userName"]}" CHANGED USER: "{userName}"s ROLE TO {newRole}',
+                    )
                     return redirect("/admin/users")
                 case False:
-                     return redirect("/")    
+                    return redirect("/")
         case False:
-            return redirect("/")    
+            return redirect("/")
--- static/js/searchBar.js
+++ static/js/searchBar.js
@@ -1,8 +1,8 @@
 function searchBar() {
-    var input = document.querySelector("#searchBarInput").value;
-    console.log(input)
-    if (input === "" || input.replace(/\s/g, "") === "") {
-    } else {
-        window.location.href = `/search/${input.replace(/\s/g, "+")}`;
-    }
+  var input = document.querySelector("#searchBarInput").value;
+  console.log(input);
+  if (input === "" || input.replace(/\s/g, "") === "") {
+  } else {
+    window.location.href = `/search/${input.replace(/\s/g, "+")}`;
+  }
 }
