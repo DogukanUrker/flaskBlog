0931c62be49a25e9a2355ae57b393a8ea494364b
Doğukan Ürker <dogukanurker@icloud.com>
2024-01-14 21:28:03 +0300

    tabler icons

--- static/tailwindUI/js/changeTheme.js
+++ static/tailwindUI/js/changeTheme.js
@@ -12,15 +12,13 @@ if (localStorage.getItem(theme) === "dark") {
 function toLight() {
   localStorage.setItem(theme, "light");
   body.className = "text-neutral-100 bg-neutral-900 selection:bg-rose-500";
-  changeTheme.innerHTML =
-    '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" /></svg>';
+  changeTheme.innerHTML = '<i class="ti ti-moon"></i>';
   changeTheme.setAttribute("onclick", "javascript: toDark();");
 }
 
 function toDark() {
   localStorage.setItem(theme, "dark");
   body.className = "text-neutral-900 bg-neutral-100 selection:bg-rose-500";
-  changeTheme.innerHTML =
-    '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" /></svg>';
+  changeTheme.innerHTML = '<i class="ti ti-sun"></i>';
   changeTheme.setAttribute("onclick", "javascript: toLight();");
 }
