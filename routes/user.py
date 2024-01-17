6909bc1c8931242c2da5b8df42d2b69961a1e86f
Doğukan Ürker <dogukanurker@icloud.com>
2024-01-17 13:22:25 +0300

    SQL cursor execute changes

--- routes/user.py
+++ routes/user.py
@@ -20,21 +20,31 @@ def user(userName):
     match str(userName).lower() in str(users).lower():
         case True:
             message("2", f'USER: "{userName}" FOUND')
-            cursor.execute(f'select * from users where lower(userName) = "{userName}"')
+            cursor.execute(
+                """select * from users where lower(userName) = ?""",
+                [(userName)],
+            )
             user = cursor.fetchone()
             connection = sqlite3.connect(DB_POSTS_ROOT)
             cursor = connection.cursor()
-            cursor.execute(f'select views from posts where author = "{user[1]}"')
+            cursor.execute(
+                """select views from posts where author = ?""",
+                [(user[1])],
+            )
             viewsData = cursor.fetchall()
             views = 0
             for view in viewsData:
                 views += int(view[0])
-            cursor.execute(f'select * from posts where author = "{user[1]}"')
+            cursor.execute(
+                """select * from posts where author = ?""",
+                [(user[1])],
+            )
             posts = cursor.fetchall()
             connection = sqlite3.connect(DB_COMMENTS_ROOT)
             cursor = connection.cursor()
             cursor.execute(
-                f'select * from comments where lower(user) = "{userName.lower()}"'
+                """select * from comments where lower(user) = ?""",
+                [(userName.lower())],
             )
             comments = cursor.fetchall()
             if posts:
