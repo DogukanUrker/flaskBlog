e6029d5e8af99a2b7f0a8987f227af35faaf73ab
Doğukan Ürker <dogukanurker@icloud.com>
2024-01-18 22:22:33 +0300

    recaptcha added to login page

--- routes/login.py
+++ routes/login.py
@@ -1,19 +1,25 @@
 from helpers import (
+    abort,
     flash,
+    LOG_IN,
     url_for,
     session,
     request,
     sqlite3,
     message,
     redirect,
+    RECAPTCHA,
     addPoints,
     Blueprint,
     loginForm,
+    requestsPost,
     sha256_crypt,
     DB_USERS_ROOT,
     render_template,
+    RECAPTCHA_SITE_KEY,
+    RECAPTCHA_VERIFY_URL,
+    RECAPTCHA_SECRET_KEY,
 )
-from constants import LOG_IN
 
 loginBlueprint = Blueprint("login", __name__)
 
@@ -51,18 +57,67 @@ def login(direct):
                                 case _:
                                     match sha256_crypt.verify(password, user[3]):
                                         case True:
-                                            session["userName"] = user[1]
-                                            addPoints(1, session["userName"])
-                                            message("2", f'USER: "{user[1]}" LOGGED IN')
-                                            flash(f"Welcome {user[1]}", "success")
-                                            return (
-                                                redirect(direct),
-                                                301,
-                                            )
+                                            match RECAPTCHA:
+                                                case True:
+                                                    secretResponse = request.form[
+                                                        "g-recaptcha-response"
+                                                    ]
+                                                    verifyResponse = requestsPost(
+                                                        url=f"{RECAPTCHA_VERIFY_URL}?secret={RECAPTCHA_SECRET_KEY}&response={secretResponse}"
+                                                    ).json()
+                                                    match verifyResponse[
+                                                        "success"
+                                                    ] == True or verifyResponse[
+                                                        "score"
+                                                    ] > 0.5:
+                                                        case True:
+                                                            message("2",f"LOGIN | VERIFICATION: {verifyResponse["success"]} | VERIFICATION SCORE: {verifyResponse["score"]}")
+                                                            session["userName"] = user[
+                                                                1
+                                                            ]
+                                                            addPoints(
+                                                                1, session["userName"]
+                                                            )
+                                                            message(
+                                                                "2",
+                                                                f'USER: "{user[1]}" LOGGED IN',
+                                                            )
+                                                            flash(
+                                                                f"Welcome {user[1]}",
+                                                                "success",
+                                                            )
+                                                            return (
+                                                                redirect(direct),
+                                                                301,
+                                                            )
+                                                        case False:
+                                                            message("1",f"LOGIN | VERIFICATION: {verifyResponse["success"]} | VERIFICATION SCORE: {verifyResponse["score"]}")
+                                                            abort(401)
+                                                case False:
+                                                    session["userName"] = user[1]
+                                                    addPoints(1, session["userName"])
+                                                    message(
+                                                        "2",
+                                                        f'USER: "{user[1]}" LOGGED IN',
+                                                    )
+                                                    flash(
+                                                        f"Welcome {user[1]}", "success"
+                                                    )
+                                                    return (
+                                                        redirect(direct),
+                                                        301,
+                                                    )
                                         case _:
                                             message("1", "WRONG PASSWORD")
                                             flash("wrong  password", "error")
-                    return render_template("login.html", form=form, hideLogin=True)
+                    return render_template(
+                        "login.html",
+                        form=form,
+                        hideLogin=True,
+                        direct = direct,
+                        siteKey=RECAPTCHA_SITE_KEY,
+                        recaptcha=RECAPTCHA,
+                    )
         case False:
             return (
                 redirect(direct),
