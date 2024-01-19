ee5ecab9c6879d3820c5d1aa5891f0bc476fbe8e
Doğukan Ürker <dogukanurker@icloud.com>
2024-01-19 23:42:25 +0300

    RECAPTCHA Verification added to 'password change'

--- routes/changePassword.py
+++ routes/changePassword.py
@@ -1,5 +1,7 @@
 from helpers import (
     flash,
+    abort,
+    message,
     session,
     sqlite3,
     request,
@@ -7,9 +9,15 @@ from helpers import (
     redirect,
     Blueprint,
     sha256_crypt,
+    RECAPTCHA,
+    requestsPost,
     DB_USERS_ROOT,
     render_template,
     changePasswordForm,
+    RECAPTCHA_SITE_KEY,
+    RECAPTCHA_VERIFY_URL,
+    RECAPTCHA_SECRET_KEY,
+    RECAPTCHA_PASSWORD_CHANGE,
 )
 
 changePasswordBlueprint = Blueprint("changePassword", __name__)
@@ -46,22 +54,69 @@ def changePassword():
                                 case True:
                                     newPassword = sha256_crypt.hash(password)
                                     connection = sqlite3.connect(DB_USERS_ROOT)
-                                    cursor = connection.cursor()
-                                    cursor.execute(
-                                        """update users set password = ? where userName = ? """,
-                                        [(newPassword), (session["userName"])],
-                                    )
-                                    connection.commit()
-                                    message(
-                                        "2",
-                                        f'USER: "{session["userName"]}" CHANGED HIS PASSWORD',
-                                    )
-                                    session.clear()
-                                    flash("you need login with new password", "success")
-                                    return redirect("/login/redirect=&")
+                                    match RECAPTCHA and RECAPTCHA_PASSWORD_CHANGE:
+                                        case True:
+                                            secretResponse = request.form[
+                                                "g-recaptcha-response"
+                                            ]
+                                            verifyResponse = requestsPost(
+                                                url=f"{RECAPTCHA_VERIFY_URL}?secret={RECAPTCHA_SECRET_KEY}&response={secretResponse}"
+                                            ).json()
+                                            match verifyResponse[
+                                                "success"
+                                            ] == True or verifyResponse["score"] > 0.5:
+                                                case True:
+                                                    message("2",f"PASSWORD CHANGE RECAPTCHA | VERIFICATION: {verifyResponse["success"]} | VERIFICATION SCORE: {verifyResponse["score"]}")
+                                                    cursor = connection.cursor()
+                                                    cursor.execute(
+                                                        """update users set password = ? where userName = ? """,
+                                                        [
+                                                            (newPassword),
+                                                            (session["userName"]),
+                                                        ],
+                                                    )
+                                                    connection.commit()
+                                                    message(
+                                                        "2",
+                                                        f'USER: "{session["userName"]}" CHANGED HIS PASSWORD',
+                                                    )
+                                                    session.clear()
+                                                    flash(
+                                                        "you need login with new password",
+                                                        "success",
+                                                    )
+                                                    return redirect("/login/redirect=&")
+                                                case False:
+                                                    message("1",f"PASSWORD CHANGE DELETE RECAPTCHA | VERIFICATION: {verifyResponse["success"]} | VERIFICATION SCORE: {verifyResponse["score"]}")
+                                                    abort(401)
+                                        case False:
+                                            cursor = connection.cursor()
+                                            cursor.execute(
+                                                        """update users set password = ? where userName = ? """,
+                                                        [
+                                                            (newPassword),
+                                                            (session["userName"]),
+                                                        ],
+                                                    )
+                                            connection.commit()
+                                            message(
+                                                        "2",
+                                                        f'USER: "{session["userName"]}" CHANGED HIS PASSWORD',
+                                                    )
+                                            session.clear()
+                                            flash(
+                                                        "you need login with new password",
+                                                        "success",
+                                                    )
+                                            return redirect("/login/redirect=&")
                         case _:
                             flash("old is password wrong", "error")
-            return render_template("changePassword.html", form=form)
+            return render_template(
+                "changePassword.html",
+                form=form,
+                siteKey=RECAPTCHA_SITE_KEY,
+                recaptcha=RECAPTCHA,
+            )
         case False:
             message("1", "USER NOT LOGGED IN")
             flash("you need login for change your password", "error")
