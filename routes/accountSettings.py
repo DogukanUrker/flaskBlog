09526f296d8ae32fe243abe9479d0a07502dd622
Doğukan Ürker <dogukanurker@icloud.com>
2024-01-19 22:35:02 +0300

    RECAPTCHA Verification added to 'account settings'

--- routes/accountSettings.py
+++ routes/accountSettings.py
@@ -1,11 +1,19 @@
 from helpers import (
+    abort,
+    message,
     request,
     sqlite3,
     session,
     redirect,
     Blueprint,
+    RECAPTCHA,
+    requestsPost,
     DB_USERS_ROOT,
     render_template,
+    RECAPTCHA_SITE_KEY,
+    RECAPTCHA_VERIFY_URL,
+    RECAPTCHA_SECRET_KEY,
+    RECAPTCHA_DELETE_USER,
 )
 from delete import deleteUser
 
@@ -25,10 +33,30 @@ def accountSettings():
             user = cursor.fetchall()
             match request.method == "POST":
                 case True:
-                    match "userDeleteButton" in request.form:
+                    match RECAPTCHA and RECAPTCHA_DELETE_USER:
                         case True:
+                            secretResponse = request.form[
+                                                "g-recaptcha-response"
+                                            ]
+                            verifyResponse = requestsPost(
+                                                url=f"{RECAPTCHA_VERIFY_URL}?secret={RECAPTCHA_SECRET_KEY}&response={secretResponse}"
+                                            ).json()
+                            match verifyResponse[
+                                                "success"
+                                            ] == True or verifyResponse[
+                                                "score"
+                                            ] > 0.5:
+                                
+                                case True:
+                                    message("2",f"USER DELETE RECAPTCHA | VERIFICATION: {verifyResponse["success"]} | VERIFICATION SCORE: {verifyResponse["score"]}")
+                                    deleteUser(user[0][0])
+                                    return redirect(f"/")
+                                case False:
+                                    message("1",f"USER DELETE RECAPTCHA | VERIFICATION: {verifyResponse["success"]} | VERIFICATION SCORE: {verifyResponse["score"]}")
+                                    abort(401)
+                        case False:
                             deleteUser(user[0][0])
                             return redirect(f"/")
-            return render_template("accountSettings.html", user=user)
+            return render_template("accountSettings.html", user=user, siteKey=RECAPTCHA_SITE_KEY, recaptcha=RECAPTCHA,)
         case False:
             return redirect("/login/redirect=&accountsettings")
