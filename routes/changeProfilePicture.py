62c6e24fbfc7a3b67287f1d4c60ce07d504ca529
Doğukan Ürker <dogukanurker@icloud.com>
2024-01-19 23:17:42 +0300

    RECAPTCHA Verification added to 'change profile picture'

--- routes/changeProfilePicture.py
+++ routes/changeProfilePicture.py
@@ -1,4 +1,5 @@
 from helpers import (
+    abort,
     flash,
     session,
     sqlite3,
@@ -6,9 +7,15 @@ from helpers import (
     message,
     redirect,
     Blueprint,
+    RECAPTCHA,
+    requestsPost,
     DB_USERS_ROOT,
     render_template,
+    RECAPTCHA_SITE_KEY,
+    RECAPTCHA_VERIFY_URL,
+    RECAPTCHA_SECRET_KEY,
     changeProfilePictureForm,
+    RECAPTCHA_PROFILE_PICTURE_CHANGE,
 )
 
 changeProfilePictureBlueprint = Blueprint("changeProfilePicture", __name__)
@@ -25,17 +32,52 @@ def changeProfilePicture():
                     newProfilePicture = f"https://api.dicebear.com/7.x/identicon/svg?seed={newProfilePictureSeed}&radius=10"
                     connection = sqlite3.connect(DB_USERS_ROOT)
                     cursor = connection.cursor()
-                    cursor.execute(
-                        """update users set profilePicture = ? where userName = ? """,
-                        [(newProfilePicture), (session["userName"])],
-                    )
-                    connection.commit()
-                    message(
-                        "2",
-                        f'USER: "{session["userName"]}" CHANGED HIS PROFILE PICTURE TO "{newProfilePicture}"',
-                    )
-                    flash("profile picture changed", "success")
-                    return redirect(f"/changeprofilepicture")
-            return render_template("changeProfilePicture.html", form=form)
+                    match RECAPTCHA and RECAPTCHA_PROFILE_PICTURE_CHANGE:
+                        case True:
+                            secretResponse = request.form[
+                                                        "g-recaptcha-response"
+                                                    ]
+                            verifyResponse = requestsPost(
+                                                        url=f"{RECAPTCHA_VERIFY_URL}?secret={RECAPTCHA_SECRET_KEY}&response={secretResponse}"
+                                                    ).json()
+                            match verifyResponse[
+                                                        "success"
+                                                    ] == True or verifyResponse[
+                                                        "score"
+                                                    ] > 0.5:
+                                case True:
+                                    message("2",f"CHANGE PROFILE PICTURE RECAPTCHA | VERIFICATION: {verifyResponse["success"]} | VERIFICATION SCORE: {verifyResponse["score"]}")
+                                    cursor.execute(
+                                        """update users set profilePicture = ? where userName = ? """,
+                                        [(newProfilePicture), (session["userName"])],
+                                    )
+                                    connection.commit()
+                                    message(
+                                        "2",
+                                        f'USER: "{session["userName"]}" CHANGED HIS PROFILE PICTURE TO "{newProfilePicture}"',
+                                    )
+                                    flash("profile picture changed", "success")
+                                    return redirect(f"/changeprofilepicture")
+                                case False:
+                                    message("1",f"CHANGE PROFILE PICTURE RECAPTCHA | VERIFICATION: {verifyResponse["success"]} | VERIFICATION SCORE: {verifyResponse["score"]}")
+                                    abort(401)
+                        case False:
+                            cursor.execute(
+                                        """update users set profilePicture = ? where userName = ? """,
+                                        [(newProfilePicture), (session["userName"])],
+                                    )
+                            connection.commit()
+                            message(
+                                        "2",
+                                        f'USER: "{session["userName"]}" CHANGED HIS PROFILE PICTURE TO "{newProfilePicture}"',
+                                    )
+                            flash("profile picture changed", "success")
+                            return redirect(f"/changeprofilepicture")
+            return render_template(
+                "changeProfilePicture.html",
+                form=form,
+                siteKey=RECAPTCHA_SITE_KEY,
+                recaptcha=RECAPTCHA,
+            )
         case False:
             return redirect("/")
