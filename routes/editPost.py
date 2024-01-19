fbb48a733eb6168311163886b6e9b078241f4109
Doğukan Ürker <dogukanurker@icloud.com>
2024-01-19 23:07:31 +0300

    RECAPTCHA Verification added to 'post edit'

--- routes/editPost.py
+++ routes/editPost.py
@@ -1,17 +1,24 @@
 from helpers import (
     flash,
+    abort,
     session,
     sqlite3,
     request,
     message,
     redirect,
     Blueprint,
+    RECAPTCHA,
     currentDate,
     currentTime,
+    requestsPost,
     DB_POSTS_ROOT,
     DB_USERS_ROOT,
     createPostForm,
     render_template,
+    RECAPTCHA_SITE_KEY,
+    RECAPTCHA_VERIFY_URL,
+    RECAPTCHA_SECRET_KEY,
+    RECAPTCHA_POST_EDIT,
 )
 
 editPostBlueprint = Blueprint("editPost", __name__)
@@ -60,38 +67,85 @@ def editPost(postID):
                                                 f'POST CONTENT NOT BE EMPTY USER: "{session["userName"]}"',
                                             )
                                         case False:
-                                            connection = sqlite3.connect(DB_POSTS_ROOT)
-                                            cursor = connection.cursor()
-                                            cursor.execute(
-                                                """update posts set title = ? where id = ? """,
-                                                (postTitle, post[0]),
-                                            )
-                                            cursor.execute(
-                                                """update posts set tags = ? where id = ? """,
-                                                (postTags, post[0]),
-                                            )
-                                            cursor.execute(
-                                                """update posts set content = ? where id = ? """,
-                                                (postContent, post[0]),
-                                            )
-                                            cursor.execute(
-                                                """update posts set lastEditDate = ? where id = ? """,
-                                                [(currentDate()), (post[0])],
-                                            )
-                                            cursor.execute(
-                                                """update posts set lastEditTime = ? where id = ? """,
-                                                [(currentTime()), (post[0])],
-                                            )
-                                            connection.commit()
-                                            message("2", f'POST: "{postTitle}" EDITED')
-                                            flash("Post edited", "success")
-                                            return redirect(f"/post/{post[0]}")
+                                            match RECAPTCHA and RECAPTCHA_POST_EDIT:
+                                                case True:
+                                                    secretResponse = request.form[
+                                                                        "g-recaptcha-response"
+                                                                    ]
+                                                    verifyResponse = requestsPost(
+                                                                        url=f"{RECAPTCHA_VERIFY_URL}?secret={RECAPTCHA_SECRET_KEY}&response={secretResponse}"
+                                                                    ).json()
+                                                    match verifyResponse[
+                                                                        "success"
+                                                                    ] == True or verifyResponse[
+                                                                        "score"
+                                                                    ] > 0.5:
+                                                        case True:
+                                                            message("2",f"POST EDIT RECAPTCHA | VERIFICATION: {verifyResponse["success"]} | VERIFICATION SCORE: {verifyResponse["score"]}")
+                                                            connection = sqlite3.connect(DB_POSTS_ROOT)
+                                                            cursor = connection.cursor()
+                                                            cursor.execute(
+                                                                """update posts set title = ? where id = ? """,
+                                                                (postTitle, post[0]),
+                                                            )
+                                                            cursor.execute(
+                                                                """update posts set tags = ? where id = ? """,
+                                                                (postTags, post[0]),
+                                                            )
+                                                            cursor.execute(
+                                                                """update posts set content = ? where id = ? """,
+                                                                (postContent, post[0]),
+                                                            )
+                                                            cursor.execute(
+                                                                """update posts set lastEditDate = ? where id = ? """,
+                                                                [(currentDate()), (post[0])],
+                                                            )
+                                                            cursor.execute(
+                                                                """update posts set lastEditTime = ? where id = ? """,
+                                                                [(currentTime()), (post[0])],
+                                                            )
+                                                            connection.commit()
+                                                            message("2", f'POST: "{postTitle}" EDITED')
+                                                            flash("Post edited", "success")
+                                                            return redirect(f"/post/{post[0]}")
+                                                        case False:
+                                                            message("1",f"POST EDIT RECAPTCHA | VERIFICATION: {verifyResponse["success"]} | VERIFICATION SCORE: {verifyResponse["score"]}")
+                                                            abort(401)
+                                                case False:
+                                                    connection = sqlite3.connect(DB_POSTS_ROOT)
+                                                    cursor = connection.cursor()
+                                                    cursor.execute(
+                                                        """update posts set title = ? where id = ? """,
+                                                        (postTitle, post[0]),
+                                                    )
+                                                    cursor.execute(
+                                                        """update posts set tags = ? where id = ? """,
+                                                        (postTags, post[0]),
+                                                    )
+                                                    cursor.execute(
+                                                        """update posts set content = ? where id = ? """,
+                                                        (postContent, post[0]),
+                                                    )
+                                                    cursor.execute(
+                                                        """update posts set lastEditDate = ? where id = ? """,
+                                                        [(currentDate()), (post[0])],
+                                                    )
+                                                    cursor.execute(
+                                                        """update posts set lastEditTime = ? where id = ? """,
+                                                        [(currentTime()), (post[0])],
+                                                    )
+                                                    connection.commit()
+                                                    message("2", f'POST: "{postTitle}" EDITED')
+                                                    flash("Post edited", "success")
+                                                    return redirect(f"/post/{post[0]}")
                             return render_template(
                                 "/editPost.html",
                                 title=post[1],
                                 tags=post[2],
                                 content=post[3],
                                 form=form,
+                                siteKey=RECAPTCHA_SITE_KEY,
+                                recaptcha=RECAPTCHA,
                             )
                         case False:
                             flash("this post not yours", "error")
