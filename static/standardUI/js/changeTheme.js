3fc3203aca2595ae8b08d1a6df82870d51ebafa4
Doğukan Ürker <dogukanurker@icloud.com>
2024-01-21 17:14:10 +0300

    Explanatory comments added

--- static/standardUI/js/changeTheme.js
+++ static/standardUI/js/changeTheme.js
@@ -1,30 +1,79 @@
-var root = document.querySelector(":root");
-var changeTheme = document.querySelector(".changeTheme");
-
-var theme;
-
-if (localStorage.getItem("theme") === "dark") {
-  toDark();
-} else if (localStorage.getItem("theme") === "light") {
-  toLight();
-} else if (localStorage.getItem("theme") === null) {
-  toDark();
+/**
+ * Selects the root element of the document.
+ * @returns {Element} The root element of the document.
+ */
+function selectDocumentRoot() {
+  return document.querySelector(":root");
 }
 
+/**
+ * Selects the element with the given class name.
+ * @param {string} className The class name of the element to select.
+ * @returns {Element} The element with the given class name, or null if no element is found.
+ */
+function selectElementByClassName(className) {
+  return document.querySelector(`.${className}`);
+}
+
+/**
+ * The current theme of the application.
+ * @type {string}
+ */
+let theme;
+
+/**
+ * Initializes the theme based on the value stored in local storage.
+ */
+function initTheme() {
+  if (localStorage.getItem("theme") === "dark") {
+    toDark();
+  } else if (localStorage.getItem("theme") === "light") {
+    toLight();
+  } else if (localStorage.getItem("theme") === null) {
+    toDark();
+  }
+}
+
+/**
+ * Changes the theme to light mode.
+ */
 function toLight() {
+  // set the theme in local storage
   localStorage.setItem("theme", "light");
-  root.style.setProperty("--themePrimary", "#000");
-  root.style.setProperty("--themeSecondary", "#fff");
-  root.style.setProperty("--themeHelper", "#303030");
-  changeTheme.innerHTML = '<i class="ti ti-sun"></i>';
-  changeTheme.setAttribute("onclick", "javascript: toDark();");
+
+  // set the CSS variables
+  selectDocumentRoot().style.setProperty("--themePrimary", "#000");
+  selectDocumentRoot().style.setProperty("--themeSecondary", "#fff");
+  selectDocumentRoot().style.setProperty("--themeHelper", "#303030");
+
+  // update the theme button
+  selectElementByClassName("changeTheme").innerHTML =
+    '<i class="ti ti-sun"></i>';
+  selectElementByClassName("changeTheme").setAttribute(
+    "onclick",
+    "javascript: toDark();"
+  );
 }
 
+/**
+ * Changes the theme to dark mode.
+ */
 function toDark() {
+  // set the theme in local storage
   localStorage.setItem("theme", "dark");
-  root.style.setProperty("--themePrimary", "#fff");
-  root.style.setProperty("--themeSecondary", "#000");
-  root.style.setProperty("--themeHelper", "#C6C6C6");
-  changeTheme.innerHTML = '<i class="ti ti-moon"></i>';
-  changeTheme.setAttribute("onclick", "javascript: toLight();");
+
+  // set the CSS variables
+  selectDocumentRoot().style.setProperty("--themePrimary", "#fff");
+  selectDocumentRoot().style.setProperty("--themeSecondary", "#000");
+  selectDocumentRoot().style.setProperty("--themeHelper", "#C6C6C6");
+
+  // update the theme button
+  selectElementByClassName("changeTheme").innerHTML =
+    '<i class="ti ti-moon"></i>';
+  selectElementByClassName("changeTheme").setAttribute(
+    "onclick",
+    "javascript: toLight();"
+  );
 }
+
+initTheme();
--- static/tailwindUI/js/changeTheme.js
+++ static/tailwindUI/js/changeTheme.js
@@ -1,15 +1,23 @@
+// Get the body element of the document
 var body = document.querySelector("#body");
+// Get the button element that changes the theme
 var changeTheme = document.querySelector(".changeTheme");
 
+// Declare a variable to store the theme
 var theme;
 
+// Check the value of the theme in the local storage
 if (localStorage.getItem("theme") === "dark") {
+  // If it is dark, call the toDark function
   toDark();
 } else if (localStorage.getItem("theme") === "light") {
+  // If it is light, call the toLight function
   toLight();
 } else if (localStorage.getItem("theme") === null) {
+  // If it is null, meaning the user has not set a preference, call the toDark function
   toDark();
 }
+
 /**
  * Changes the theme of the website to light mode.
  * @function
@@ -17,18 +25,18 @@ if (localStorage.getItem("theme") === "dark") {
 function toLight() {
   "use strict";
 
-  // Set the theme in local storage
+  // Set the theme in local storage to light
   localStorage.setItem("theme", "light");
 
-  // Update the CSS class of the body element
+  // Update the CSS class of the body element to use light colors
   document.body.className =
     "text-neutral-900 bg-neutral-100 selection:bg-neutral-200 selection:text-rose-500";
 
-  // Update the HTML content of the button
+  // Update the HTML content of the button to show a sun icon
   document.querySelector(".changeTheme").innerHTML =
     '<i class="ti ti-sun"></i>';
 
-  // Update the onclick event of the button
+  // Update the onclick event of the button to call the toDark function when clicked
   document
     .querySelector(".changeTheme")
     .setAttribute("onclick", "javascript: toDark();");
@@ -41,18 +49,18 @@ function toLight() {
 function toDark() {
   "use strict";
 
-  // Set the theme in local storage
+  // Set the theme in local storage to dark
   localStorage.setItem("theme", "dark");
 
-  // Update the CSS class of the body element
+  // Update the CSS class of the body element to use dark colors
   document.body.className =
     "text-neutral-100 bg-neutral-900 selection:bg-neutral-800 selection:text-rose-500";
 
-  // Update the HTML content of the button
+  // Update the HTML content of the button to show a moon icon
   document.querySelector(".changeTheme").innerHTML =
     '<i class="ti ti-moon"></i>';
 
-  // Update the onclick event of the button
+  // Update the onclick event of the button to call the toLight function when clicked
   document
     .querySelector(".changeTheme")
     .setAttribute("onclick", "javascript: toLight();");
