af313cd4f7ed61606f16df9bf48ea70992f6a47f
Doğukan Ürker <dogukanurker@icloud.com>
2024-01-21 17:14:23 +0300

    Explanatory comments added

--- static/tailwindUI/js/searchBar.js
+++ static/tailwindUI/js/searchBar.js
@@ -1,6 +1,11 @@
+/**
+ *
+ * @param {string} input
+ */
 function searchBar() {
   const input = document.querySelector("#searchBarInput").value;
   if (input === "" || input.trim() === "") {
+    // do litteraly nothing
   } else {
     window.location.href = `/search/${encodeURIComponent(
       escape(input.trim())
