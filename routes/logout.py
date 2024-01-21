43583084c16e6ff11880d21fdac5586bf736a504
Doğukan Ürker <dogukanurker@icloud.com>
2024-01-21 17:28:16 +0300

    Explanatory comments added

--- routes/logout.py
+++ routes/logout.py
@@ -1,4 +1,9 @@
-from helpers import session, redirect, message, Blueprint
+"""
+This file contains the blueprint for the logout page.
+
+The functions and methods used in this blueprint are imported from the helpers module.
+"""
+from helpers import flash, message, Blueprint, session, redirect
 
 
 logoutBlueprint = Blueprint("logout", __name__)
@@ -6,11 +11,20 @@ logoutBlueprint = Blueprint("logout", __name__)
 
 @logoutBlueprint.route("/logout")
 def logout():
-    match "userName" in session:
-        case True:
-            message("2", f'USER: "{session["userName"]}" LOGGED OUT')
-            session.clear()
-            return redirect("/")
-        case False:
-            message("1", f"USER NOT LOGGED IN")
-            return redirect("/")
+    """
+    This function handles the logout process.
+
+    It checks if the user is logged in. If they are, their session is cleared and they are redirected
+    to the homepage. A message is also displayed indicating that the user has been logged out.
+
+    If the user is not logged in, a message is displayed indicating that they are not logged in.
+    The user is then redirected to the homepage.
+    """
+    if "userName" in session:
+        message("2", f"USER: {session['userName']} LOGGED OUT")
+        session.clear()
+        flash("logged out", "error")
+        return redirect("/")
+    else:
+        message("1", "USER NOT LOGGED IN")
+        return redirect("/")
