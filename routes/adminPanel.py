571b5440fddf9d5d0f0e41e2c93979ddd5805fd5
Doğukan Ürker <dogukanurker@icloud.com>
2024-01-21 18:20:52 +0300

    Explanatory comments added

--- routes/adminPanel.py
+++ routes/adminPanel.py
@@ -1,3 +1,4 @@
+# Import the necessary modules and functions
 from helpers import (
     sqlite3,
     session,
@@ -7,13 +8,16 @@ from helpers import (
     render_template,
 )
 
+# Create a blueprint for the admin panel route
 adminPanelBlueprint = Blueprint("adminPanel", __name__)
 
 
 @adminPanelBlueprint.route("/admin")
 def adminPanel():
+    # Check if the user is logged in
     match "userName" in session:
         case True:
+            # Connect to the database and get the user role
             connection = sqlite3.connect(DB_USERS_ROOT)
             cursor = connection.cursor()
             cursor.execute(
@@ -21,10 +25,14 @@ def adminPanel():
                 [(session["userName"])],
             )
             role = cursor.fetchone()[0]
+            # Check if the user role is admin
             match role == "admin":
                 case True:
+                    # Render the admin panel template
                     return render_template("adminPanel.html")
                 case False:
+                    # Redirect to the home page
                     return redirect("/")
         case False:
+            # Redirect to the login page
             return redirect("/")
