9f03b7bf479aa82c2396cb6b007d589d769d8ba2
Doğukan Ürker <dogukanurker@icloud.com>
2024-01-21 17:14:19 +0300

    Explanatory comments added

--- static/tailwindUI/js/recaptcha.js
+++ static/tailwindUI/js/recaptcha.js
@@ -1,3 +1,7 @@
+/**
+ * @param {string} token The reCAPTCHA token.
+ * @description This function is called when the reCAPTCHA form is submitted.
+ */
 function onSubmit(token) {
   document.getElementById("recaptchaForm").submit();
 }
