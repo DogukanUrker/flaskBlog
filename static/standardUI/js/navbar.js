dffe2a952901e34fff8fd771fb63c094e1750108
Doğukan Ürker <dogukanurker@icloud.com>
2024-01-21 17:14:26 +0300

    Explanatory comments added

--- static/standardUI/js/navbar.js
+++ static/standardUI/js/navbar.js
@@ -1,17 +1,53 @@
+/**
+ * Toggles the visibility of the hamburger dropdown menu
+ * @param {Event} event - The click event that triggered the function
+ */
+function hamburger(event) {
+  /**
+   * The element that contains the hamburger dropdown menu
+   * @type {HTMLElement}
+   */
+  const hamburgerDropdown = document.getElementById("hamburgerDropdown");
+
+  /**
+   * Toggles the class "show" on the hamburger dropdown menu
+   */
+  hamburgerDropdown.classList.toggle("show");
+}
+
+/**
+ * Redirects the user to the search results page when the search button is clicked
+ */
 function search() {
-  const input = document.querySelector("#searchInput").value;
-  if (input === "" || input.trim() === "") {
+  /**
+   * The input element for the search query
+   * @type {HTMLInputElement}
+   */
+  const input = document.querySelector("#searchInput");
+
+  /**
+   * The value of the search query
+   * @type {string}
+   */
+  const inputValue = input.value;
+
+  /**
+   * The URL for the search results page
+   * @type {string}
+   */
+  const searchUrl = `/search/${encodeURIComponent(escape(inputValue))}`;
+
+  if (inputValue === "" || inputValue.trim() === "") {
+    // do nothing
   } else {
-    window.location.href = `/search/${encodeURIComponent(
-      escape(input.trim())
-    )}`;
+    window.location.href = searchUrl;
   }
 }
 
-function hamburger() {
-  document.getElementById("hamburgerDropdown").classList.toggle("show");
-}
-
+/**
+ * Closes the hamburger dropdown menu when a click event occurs outside of the menu
+ * @param {Event} event - The click event that triggered the function
+ */
 window.onclick = function(event) {
   if (!event.target.matches(".hamburgerBtn")) {
     var dropdowns = document.getElementsByClassName("hamburgerContent");
--- static/tailwindUI/js/navbar.js
+++ static/tailwindUI/js/navbar.js
@@ -1,26 +1,14 @@
+/**
+ *
+ * @param {string} input
+ */
 function search() {
   const input = document.querySelector("#searchInput").value;
   if (input === "" || input.trim() === "") {
+    // do litteraly nothing
   } else {
     window.location.href = `/search/${encodeURIComponent(
       escape(input.trim())
     )}`;
   }
 }
-
-function hamburger() {
-  document.getElementById("hamburgerDropdown").classList.toggle("show");
-}
-
-window.onclick = function(event) {
-  if (!event.target.matches(".hamburgerBtn")) {
-    var dropdowns = document.getElementsByClassName("hamburgerContent");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};
