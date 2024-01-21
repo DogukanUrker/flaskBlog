bc0e5cc230b07d01accff882d608db19904118d3
Doğukan Ürker <dogukanurker@icloud.com>
2024-01-21 17:14:15 +0300

    Explanatory comments added

--- static/standardUI/js/editor.js
+++ static/standardUI/js/editor.js
@@ -1,3 +1,8 @@
+/**
+ * Initialize the Summernote WYSIWYG editor on the given element.
+ *
+ * @param {JQuery} element - The element to initialize the editor on
+ */
 $(document).ready(function() {
   $(".editor").summernote({
     height: 520,
--- static/tailwindUI/js/editor.js
+++ static/tailwindUI/js/editor.js
@@ -1,5 +1,10 @@
+/**
+ * Initialize the Summernote WYSIWYG editor on the given element.
+ *
+ * @param {JQuery} element - The element to initialize the editor on
+ */
 $(document).ready(function() {
-  $("#editor").summernote({
+  $(".editor").summernote({
     height: 520,
   });
 });
