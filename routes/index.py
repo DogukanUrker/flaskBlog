1ad8b53c438bac456e512d2b52862024abc147df
Doğukan Ürker <dogukanurker@icloud.com>
2024-01-21 17:28:25 +0300

    Explanatory comments added

--- routes/index.py
+++ routes/index.py
@@ -1,3 +1,16 @@
+"""
+This file contains the routes for the Flask application.
+
+The Blueprint "index" is used to define the home page of the application.
+The route "/" maps the index function to the home page.
+
+The index function retrieves all posts from the database and passes them to the index.html template.
+
+The posts variable is passed to the index.html template as a list of dictionaries.
+
+The index.html template displays the title and content of each post.
+"""
+
 from helpers import (
     sqlite3,
     Blueprint,
@@ -10,11 +23,17 @@ indexBlueprint = Blueprint("index", __name__)
 
 @indexBlueprint.route("/")
 def index():
+    """
+    This function maps the home page route ("/") to the index function.
+
+    It retrieves all posts from the database and passes them to the index.html template.
+
+    The posts variable is passed to the index.html template as a list of dictionaries.
+
+    The index.html template displays the title and content of each post.
+    """
     connection = sqlite3.connect(DB_POSTS_ROOT)
     cursor = connection.cursor()
     cursor.execute("select * from posts")
     posts = cursor.fetchall()
-    return render_template(
-        "index.html",
-        posts=posts,
-    )
+    return render_template("index.html", posts=posts)
