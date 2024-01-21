f1078e081a565ce9c688d33cddbbc4e4945d704c
Doğukan Ürker <dogukanurker@icloud.com>
2024-01-21 18:54:08 +0300

    Explanatory comments added

--- routes/searchBar.py
+++ routes/searchBar.py
@@ -1,11 +1,20 @@
+# Import the necessary modules and functions
 from helpers import (
     Blueprint,
     render_template,
 )
 
+
+# Create a blueprint for the search bar route
 searchBarBlueprint = Blueprint("searchBar", __name__)
 
 
 @searchBarBlueprint.route("/searchbar")
 def searchBar():
+    """
+    This function returns the search bar HTML page.
+
+    Returns:
+        The search bar HTML page.
+    """
     return render_template("searchBar.html")
