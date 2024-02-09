2a11655cea0610c512426bdb5a8ecfd81446bccb
Doğukan Ürker <dogukanurker@icloud.com>
2024-02-10 02:04:51 +0300

    docs: add more comments

--- utils/time.py
+++ utils/time.py
@@ -1,3 +1,6 @@
+"""
+Importing necessary modules.
+"""
 from modules import tzname, datetime
 
 
@@ -6,7 +9,7 @@ def currentTimeZone():
     """
     Returns the current system time zone as a string.
     """
-    return tzname[0]
+    return tzname[0]  # Return the first element of the tzname list, which typically represents the time zone name.
 
 
 # Function to get the current date in the format "dd.mm.yy"
@@ -14,7 +17,7 @@ def currentDate():
     """
     Returns the current date as a string in the format "dd.mm.yy".
     """
-    return datetime.now().strftime("%d.%m.%y")
+    return datetime.now().strftime("%d.%m.%y")  # Return current date formatted as day.month.year.
 
 
 # Function to get the current time
@@ -22,15 +25,15 @@ def currentTime(seconds=False, microSeconds=False):
     """
     Returns the current time as a string in the format "HH:MM" or "HH:MM:SS" depending on the value of the seconds parameter. If the microSeconds parameter is set to True, the time will include microseconds as well.
     """
-    match seconds:
+    match seconds:  # Using pattern matching to handle different cases
         case False:
-            return datetime.now().strftime("%H:%M")
+            return datetime.now().strftime("%H:%M")  # Return current time formatted as hour:minute.
         case True:
-            match microSeconds:
+            match microSeconds:  # Nested pattern matching to handle microseconds
                 case True:
-                    return datetime.now().strftime("%H:%M:%S.%f")
+                    return datetime.now().strftime("%H:%M:%S.%f")  # Return current time with microseconds.
                 case False:
-                    return datetime.now().strftime("%H:%M:%S")
+                    return datetime.now().strftime("%H:%M:%S")  # Return current time with seconds.
 
 
 # Function to get the current timestamp
@@ -38,6 +41,6 @@ def currentTimeStamp():
     """
     Returns the current time stamp as an integer.
     """
-    timeStamp = datetime.now().timestamp()  # Get current timestamp
-    timeStamp = int(timeStamp)  # Convert to integer
-    return timeStamp
+    timeStamp = datetime.now().timestamp()  # Get current timestamp using datetime module.
+    timeStamp = int(timeStamp)  # Convert timestamp to integer.
+    return timeStamp  # Return the integer timestamp.
