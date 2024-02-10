132819df7f09c17a5464bf7dafca8a9a442dc060
Doğukan Ürker <dogukanurker@icloud.com>
2024-02-10 21:46:26 +0300

    docs: add more comments

--- routes/editPost.py
+++ routes/editPost.py
@@ -1,30 +1,31 @@
-# Import the necessary modules and functions
+# Import necessary modules and functions
 from modules import (
-    Log,
-    flash,
-    abort,
-    session,
-    sqlite3,
-    request,
-    redirect,
-    Blueprint,
-    RECAPTCHA,
-    requestsPost,
-    DB_POSTS_ROOT,
-    DB_USERS_ROOT,
-    CreatePostForm,
-    render_template,
-    currentTimeStamp,
-    RECAPTCHA_SITE_KEY,
-    RECAPTCHA_POST_EDIT,
-    RECAPTCHA_VERIFY_URL,
-    RECAPTCHA_SECRET_KEY,
+    Log,  # Logging module
+    flash,  # Flash messaging module
+    abort,  # Function for aborting requests
+    session,  # Session management module
+    sqlite3,  # SQLite database module
+    request,  # Module for handling HTTP requests
+    redirect,  # Function for redirecting requests
+    Blueprint,  # Blueprint class for creating modular applications
+    RECAPTCHA,  # Recaptcha module
+    requestsPost,  # Module for making HTTP POST requests
+    DB_POSTS_ROOT,  # Path to the posts database
+    DB_USERS_ROOT,  # Path to the users database
+    CreatePostForm,  # Form for creating a post
+    render_template,  # Function for rendering templates
+    currentTimeStamp,  # Function for getting current timestamp
+    RECAPTCHA_SITE_KEY,  # Recaptcha site key
+    RECAPTCHA_POST_EDIT,  # # Flag for enabling/disabling Recaptcha for post editing
+    RECAPTCHA_VERIFY_URL,  # Recaptcha verification URL
+    RECAPTCHA_SECRET_KEY,  # Recaptcha secret key
 )
 
 # Create a blueprint for the edit post route
 editPostBlueprint = Blueprint("editPost", __name__)
 
 
+# Define a route for editing a post
 @editPostBlueprint.route("/editpost/<int:postID>", methods=["GET", "POST"])
 def editPost(postID):
     """
@@ -40,14 +41,18 @@ def editPost(postID):
         abort(404): if the post does not exist
         abort(401): if the user is not authorized to edit the post
     """
+    # Check if "userName" exists in session
     match "userName" in session:
         case True:
+            # Connect to the posts database
             connection = sqlite3.connect(DB_POSTS_ROOT)
             cursor = connection.cursor()
             cursor.execute("select id from posts")
             posts = str(cursor.fetchall())
+            # Check if postID exists in posts
             match str(postID) in posts:
                 case True:
+                    # Connect to the posts database
                     connection = sqlite3.connect(DB_POSTS_ROOT)
                     cursor = connection.cursor()
                     cursor.execute(
@@ -56,28 +61,34 @@ def editPost(postID):
                     )
                     post = cursor.fetchone()
                     Log.success(f'POST: "{postID}" FOUND')
+                    # Connect to the users database
                     connection = sqlite3.connect(DB_USERS_ROOT)
                     cursor = connection.cursor()
                     cursor.execute(
                         """select userName from users where userName = ? """,
                         [(session["userName"])],
                     )
+                    # Check if the user is authorized to edit the post
                     match post[5] == session["userName"] or session[
                         "userRole"
                     ] == "admin":
                         case True:
+                            # Populate the form with post data
                             form = CreatePostForm(request.form)
                             form.postTitle.data = post[1]
                             form.postTags.data = post[2]
                             form.postContent.data = post[3]
                             form.postCategory.data = post[9]
+                            # Check if the request method is POST
                             match request.method == "POST":
                                 case True:
+                                    # Retrieve post data from the form
                                     postTitle = request.form["postTitle"]
                                     postTags = request.form["postTags"]
                                     postContent = request.form["postContent"]
                                     postCategory = request.form["postCategory"]
                                     postBanner = request.files["postBanner"].read()
+                                    # Check if post content is empty
                                     match postContent == "":
                                         case True:
                                             flash("post content not be empty", "error")
@@ -85,23 +96,24 @@ def editPost(postID):
                                                 f'POST CONTENT NOT BE EMPTY USER: "{session["userName"]}"',
                                             )
                                         case False:
+                                            # Check Recaptcha if enabled
                                             match RECAPTCHA and RECAPTCHA_POST_EDIT:
                                                 case True:
+                                                    # Verify Recaptcha response
                                                     secretResponse = request.form[
                                                         "g-recaptcha-response"
                                                     ]
                                                     verifyResponse = requestsPost(
                                                         url=f"{RECAPTCHA_VERIFY_URL}?secret={RECAPTCHA_SECRET_KEY}&response={secretResponse}"
                                                     ).json()
+                                                    # Check Recaptcha verification result
                                                     match verifyResponse[
                                                         "success"
                                                     ] == True or verifyResponse[
                                                         "score"
                                                     ] > 0.5:
                                                         case True:
-                                                            Log.success(
-                                                                f"POST EDIT RECAPTCHA | VERIFICATION: {verifyResponse['success']} | VERIFICATION SCORE: {verifyResponse['score']}",
-                                                            )
+                                                            # Update post data in the database
                                                             connection = (
                                                                 sqlite3.connect(
                                                                     DB_POSTS_ROOT
@@ -148,11 +160,13 @@ def editPost(postID):
                                                                 f"/post/{post[0]}"
                                                             )
                                                         case False:
+                                                            # Recaptcha verification failed
                                                             Log.danger(
                                                                 f"POST EDIT RECAPTCHA | VERIFICATION: {verifyResponse['success']} | VERIFICATION SCORE: {verifyResponse['score']}",
                                                             )
                                                             abort(401)
                                                 case False:
+                                                    # Recaptcha not enabled
                                                     connection = sqlite3.connect(
                                                         DB_POSTS_ROOT
                                                     )
@@ -190,6 +204,7 @@ def editPost(postID):
                                                     )
                                                     flash("Post edited", "success")
                                                     return redirect(f"/post/{post[0]}")
+                            # Render the edit post template
                             return render_template(
                                 "/editPost.html.jinja",
                                 id=post[0],
@@ -201,15 +216,18 @@ def editPost(postID):
                                 recaptcha=RECAPTCHA,
                             )
                         case False:
+                            # User is not authorized to edit the post
                             flash("this post not yours", "error")
                             Log.danger(
                                 f'THIS POST DOES NOT BELONG TO USER: "{session["userName"]}"',
                             )
                             return redirect("/")
                 case False:
+                    # Post with postID does not exist
                     Log.danger(f'POST: "{postID}" NOT FOUND')
                     return render_template("notFound.html.jinja")
         case False:
+            # User is not logged in
             Log.danger("USER NOT LOGGED IN")
             flash("you need login for edit a post", "error")
             return redirect(f"/login/redirect=&editpost&{postID}")
