aef194cfcc735cd2b959e1a8497bf910216707f1
Doğukan Ürker <dogukanurker@icloud.com>
2024-02-10 21:46:30 +0300

    Update createPost.py

--- routes/createPost.py
+++ routes/createPost.py
@@ -1,30 +1,31 @@
-# Import the necessary modules and functions
+# Import necessary modules and functions
 from modules import (
-    Log,
-    flash,
-    abort,
-    session,
-    sqlite3,
-    request,
-    redirect,
-    addPoints,
-    Blueprint,
-    RECAPTCHA,
-    requestsPost,
-    DB_POSTS_ROOT,
-    CreatePostForm,
-    render_template,
-    currentTimeStamp,
-    RECAPTCHA_SITE_KEY,
-    RECAPTCHA_VERIFY_URL,
-    RECAPTCHA_SECRET_KEY,
-    RECAPTCHA_POST_CREATE,
+    Log,  # Logging module
+    flash,  # Flash messaging module
+    abort,  # Function for aborting requests
+    session,  # Session management module
+    sqlite3,  # SQLite database module
+    request,  # Module for handling HTTP requests
+    redirect,  # Function for redirecting requests
+    addPoints,  # Function for adding points to a user
+    Blueprint,  # Blueprint class for creating modular applications
+    RECAPTCHA,  # Recaptcha module
+    requestsPost,  # Module for making HTTP POST requests
+    DB_POSTS_ROOT,  # Path to the posts database
+    CreatePostForm,  # Form for creating a post
+    render_template,  # Function for rendering templates
+    currentTimeStamp,  # Function for getting current timestamp
+    RECAPTCHA_SITE_KEY,  # Recaptcha site key
+    RECAPTCHA_VERIFY_URL,  # Recaptcha verification URL
+    RECAPTCHA_SECRET_KEY,  # Recaptcha secret key
+    RECAPTCHA_POST_CREATE,  # Flag for enabling/disabling Recaptcha for post creation
 )
 
 # Create a blueprint for the create post route
 createPostBlueprint = Blueprint("createPost", __name__)
 
 
+# Define a route for creating a post
 @createPostBlueprint.route("/createpost", methods=["GET", "POST"])
 def createPost():
     """
@@ -39,38 +40,45 @@ def createPost():
     Raises:
         401: If the user is not authenticated.
     """
+    # Check if "userName" exists in session
     match "userName" in session:
         case True:
+            # Create form instance
             form = CreatePostForm(request.form)
+            # Check if the request method is POST
             match request.method == "POST":
                 case True:
+                    # Retrieve post data from the form
                     postTitle = request.form["postTitle"]
                     postTags = request.form["postTags"]
                     postContent = request.form["postContent"]
                     postBanner = request.files["postBanner"].read()
                     postCategory = request.form["postCategory"]
+                    # Check if post content is empty
                     match postContent == "":
                         case True:
+                            # Flash an error message
                             flash("post content not be empty", "error")
                             Log.danger(
                                 f'POST CONTENT NOT BE EMPTY USER: "{session["userName"]}"',
                             )
                         case False:
+                            # Check Recaptcha if enabled
                             match RECAPTCHA and RECAPTCHA_POST_CREATE:
                                 case True:
+                                    # Verify Recaptcha response
                                     secretResponse = request.form[
                                         "g-recaptcha-response"
                                     ]
                                     verifyResponse = requestsPost(
                                         url=f"{RECAPTCHA_VERIFY_URL}?secret={RECAPTCHA_SECRET_KEY}&response={secretResponse}"
                                     ).json()
+                                    # Check Recaptcha verification result
                                     match verifyResponse[
                                         "success"
                                     ] == True or verifyResponse["score"] > 0.5:
                                         case True:
-                                            Log.success(
-                                                f"POST CREATE RECAPTCHA | VERIFICATION: {verifyResponse['success']} | VERIFICATION SCORE: {verifyResponse['score']}",
-                                            )
+                                            # Insert new post into the database
                                             connection = sqlite3.connect(DB_POSTS_ROOT)
                                             cursor = connection.cursor()
                                             cursor.execute(
@@ -92,6 +100,7 @@ def createPost():
                                             Log.success(
                                                 f'POST: "{postTitle}" POSTED BY "{session["userName"]}"',
                                             )
+                                            # Award points to the user for posting
                                             addPoints(20, session["userName"])
                                             flash(
                                                 "You earned 20 points by posting ",
@@ -99,11 +108,13 @@ def createPost():
                                             )
                                             return redirect("/")
                                         case False:
+                                            # Recaptcha verification failed
                                             Log.danger(
                                                 f"POST CREATE RECAPTCHA | VERIFICATION: {verifyResponse['success']} | VERIFICATION SCORE: {verifyResponse['score']}",
                                             )
                                             abort(401)
                                 case False:
+                                    # Recaptcha not enabled
                                     connection = sqlite3.connect(DB_POSTS_ROOT)
                                     cursor = connection.cursor()
                                     cursor.execute(
@@ -125,9 +136,11 @@ def createPost():
                                     Log.success(
                                         f'POST: "{postTitle}" POSTED BY "{session["userName"]}"',
                                     )
+                                    # Award points to the user for posting
                                     addPoints(20, session["userName"])
                                     flash("You earned 20 points by posting ", "success")
                                     return redirect("/")
+            # Render the create post template
             return render_template(
                 "createPost.html.jinja",
                 form=form,
@@ -135,6 +148,7 @@ def createPost():
                 recaptcha=RECAPTCHA,
             )
         case False:
+            # User is not logged in
             Log.danger("USER NOT LOGGED IN")
             flash("you need loin for create a post", "error")
             return redirect("/login/redirect=&createpost")
