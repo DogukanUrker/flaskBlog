dfd2e11d39f97053d290d6f1882aba8494c93a08
Doğukan Ürker <dogukanurker@icloud.com>
2024-07-23 01:37:32 +0300

    feat( #80 ): app translated to 🇵🇱Polish

--- constants.py
+++ constants.py
@@ -40,7 +40,7 @@ LOG_IN = True  # (bool)
 REGISTRATION = True  # (bool)
 
 # Supported languages for the application
-LANGUAGES = ["en", "tr", "es", "de", "zh", "fr", "uk", "ru", "pt", "ja"]  # (list)
+LANGUAGES = ["en", "tr", "es", "de", "zh", "fr", "uk", "ru", "pt", "ja", "pl"]  # (list)
 
 
 ### LOGGER SETTINGS ###
