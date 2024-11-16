3e3b97f27fcf6d5e50fd4547ab0ff0edbadef24d
Doğukan Ürker <dogukanurker@icloud.com>
2024-11-16 20:26:37 +0300

    fix: a few optimization for #113

--- utils/generateUrlIdFromPost.py
+++ utils/generateUrlIdFromPost.py
@@ -1,41 +1,29 @@
-"""
-This modules contains code of post title to url id generator 
-"""
-
-from modules import (
-    DB_POSTS_ROOT,  # Root directory path for the posts database
-    Log,
-    sqlite3,
-)
+from modules import DB_POSTS_ROOT, Log, sqlite3, uuid
 
 
-# function to check url id exist or not in database
-def checkIfUrlIdExistPostDb(urlId):
+# Function to check if URL ID exists in the database
+def checkIfUrlIdExistsInPostDb(urlId):
     with sqlite3.connect(DB_POSTS_ROOT) as connection:
         connection.set_trace_callback(Log.sql)
         cursor = connection.cursor()
-        s = cursor.execute("""SELECT urlId from posts WHERE urlId = ? """, (urlId,))
-        result = s.fetchall()
-        if result:
-            return True  # urlId already exist in db
-        else:
-            return False  # urlId does not exist in db
+        cursor.execute("SELECT urlId FROM posts WHERE urlId = ?", (urlId,))
+        return bool(cursor.fetchall())
 
 
-# characters to remove for post title that causes issue
-avoid_characters = [
+# Characters to remove for post title that causes issues
+AVOID_CHARACTERS = [
     " ",  # Space
-    "<",
-    ">",  # Angle brackets
+    "<",  # Less than
+    ">",  # Greater than
     "#",  # Hash
     "%",  # Percent
-    "{",
+    "{",  # Curly braces
     "}",  # Curly braces
     "|",  # Pipe
     "\\",  # Backslash
     "^",  # Caret
     "~",  # Tilde
-    "[",
+    "[",  # Square brackets
     "]",  # Square brackets
     "`",  # Backtick
     '"',  # Double quote
@@ -46,58 +34,42 @@ avoid_characters = [
     "?",  # Question mark
     "=",  # Equals
     "&",  # Ampersand
-    "@",  # At symbol
-    "+",  # Plus sign
-    ".",  # full stop
-    ",",  # comma
-]
-
-# sample text
-text = " 'Hello, world!' said the coder. <Can you solve this?> {It's 50% complete.} Use |pipe|, \\, ^caret^, ~tilde~, [brackets], `backtick`, 'single quote', :colon:, ;semicolon;, /slash/, ?question?, =equals=, &ampersand&, @at@, +plus+."
+    "@",  # At
+    "+",  # Plus
+    ".",  # Period
+    ",",  # Comma
+]  # Characters to avoid in post title for URL ID generation function to work properly. Add more characters if needed.
 
 
+# Function to generate a new unique ID
 def getNewUid():
-    import uuid
-
-    uniqueID = str(uuid.uuid4())[24:]
-    return uniqueID
+    return str(uuid.uuid4())[24:]
 
 
-def convertPostTitleInUrlId(postTitle):
-    cleanedTitle = postTitle
-    filteredWords = []
-    final_url = ""
-
-    # Replace avoidable characters with a hyphen
-    for char in avoid_characters:
-        cleanedTitle = cleanedTitle.replace(char, "-")
-
-    # Split the string into words using hyphen as a separator
-    filteredWords = cleanedTitle.split("-")
-
-    # Reconstruct the string, avoiding empty strings
-    for word in filteredWords:
-        if word:  # Skip empty words
-            final_url = f"{final_url}-{word}"
-
-    # Generate the final URL ID by appending a unique ID
-    url_id = f"{final_url.strip('-')}-{getNewUid()}".lower()
-
-    return url_id
+# Function to convert post title to URL ID
+def convertPostTitleToUrlId(postTitle):
+    cleanedTitle = "".join(
+        ["-" if char in AVOID_CHARACTERS else char for char in postTitle]
+    )
+    filteredWords = [word for word in cleanedTitle.split("-") if word]
+    finalUrl = "-".join(filteredWords)
+    return f"{finalUrl}-{getNewUid()}".lower()
 
 
+# Function to generate a unique URL ID
 def generateUrlId(postTitle):
-    baseUrlId = convertPostTitleInUrlId(postTitle)
+    baseUrlId = convertPostTitleToUrlId(postTitle)
     newUrlId = baseUrlId
     counter = 1
 
-    # Check if the ID exists and modify it if necessary
-    while checkIfUrlIdExistPostDb(newUrlId):
+    while checkIfUrlIdExistsInPostDb(newUrlId):
         newUrlId = f"{baseUrlId}-{counter}"
         counter += 1
 
     return newUrlId
 
 
+# Test the function.
 if __name__ == "__main__":
-    convertPostTitleInUrlId(text)
+    sampleText = " 'Hello, world!' said the coder. <Can you solve this?> {It's 50% complete.} Use |pipe|, \\, ^caret^, ~tilde~, [brackets], `backtick`, 'single quote', :colon:, ;semicolon;, /slash/, ?question?, =equals=, &ampersand&, @at@, +plus+."
+    print(convertPostTitleToUrlId(sampleText))
