6e85fd33c37205db59bd814f6e0b83aeb0faa00e
Doğukan Ürker <dogukanurker@icloud.com>
2024-12-05 12:38:29 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -10,7 +10,7 @@ from modules import secrets, socket
 APP_NAME = "flaskBlog"  # (str)
 
 # Version of the Flask application
-APP_VERSION = "2.2.1"  # (str)
+APP_VERSION = "2.3.0"  # (str)
 
 # Path to the root of the application files
 APP_ROOT_PATH = "."  # (str)
