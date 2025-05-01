b01266518d510b11192d9a1a674aa81e35455406
Rohit Vishwakarma <rv20823@gmail.com>
2025-05-01 15:44:45 +0530

    Fix timeStamp breaks from python side

--- routes/returnHomeFeedData.py
+++ routes/returnHomeFeedData.py
@@ -7,6 +7,7 @@ from modules import (
     url_for,
     getProfilePicture,
     request,
+    datetime
 )
 
 returnHomeFeedDataBlueprint = Blueprint("returnHomeFeedData", __name__)
@@ -41,7 +42,7 @@ def homeFeedData():
             homeFeedObj["title"] = data[1]  # Post Title
             homeFeedObj["content"] = data[2]  # Post Content
             homeFeedObj["author"] = data[3]  # Author Name
-            homeFeedObj["timeStamp"] = data[4]  # Timestamp
+            homeFeedObj["timeStamp"] = datetime.fromtimestamp(data[4]).strftime("%d.%m.%Y")  # Timestamp
             homeFeedObj["category"] = data[5]  # Post Category
             homeFeedObj["urlID"] = data[6]  # URL ID
 
